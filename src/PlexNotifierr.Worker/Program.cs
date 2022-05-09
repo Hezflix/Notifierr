b26791dc4d04d1e9cbaff35c60e8ba5738618722
Bastien Riviere <me@babariviere.com>
2022-05-09 23:01:57 +0200

    chore: setup PlexApi dependency
    Create basic host and services in the worker.

--- src/PlexNotifierr.Worker/Program.cs
+++ src/PlexNotifierr.Worker/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Plex.Api.Factories;
+using Plex.Library.Factories;
+using Plex.ServerApi;
+using Plex.ServerApi.Api;
+using Plex.ServerApi.Clients;
+using Plex.ServerApi.Clients.Interfaces;
+
+
+// Create Client Options
+var apiOptions = new ClientOptions
+{
+    Product = "Notifierr",
+    DeviceName = "Notifierr",
+    ClientId = "MyClientId",
+    Platform = "Web",
+    Version = "v1"
+};
+
+using IHost host = Host.CreateDefaultBuilder()
+    .ConfigureServices((_, services) =>
+                       services.AddSingleton(apiOptions)
+                       .AddTransient<IPlexServerClient, PlexServerClient>()
+                       .AddTransient<IPlexAccountClient, PlexAccountClient>()
+                       .AddTransient<IPlexLibraryClient, PlexLibraryClient>()
+                       .AddTransient<IApiService, ApiService>()
+                       .AddTransient<IPlexFactory, PlexFactory>()
+                       .AddTransient<IPlexRequestsHttpClient, PlexRequestsHttpClient>())
+    .Build();
+
+host.Run();
