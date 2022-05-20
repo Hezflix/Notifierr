64c200e01617a2cdf7561cb7be78cd10c923ea0a
Kevin Harismendy <kharismendy@veepee.com>
2022-05-20 15:24:54 +0200

    fix: GetUsersHistory job per user
    Bump Plex.Apito 3.0.1

--- src/PlexNotifierr.Worker/Jobs/GetUsersHistoryJob.cs
+++ src/PlexNotifierr.Worker/Jobs/GetUsersHistoryJob.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Plex.ServerApi.Clients.Interfaces;
-using Plex.ServerApi.PlexModels.Media;
 using PlexNotifierr.Core.Models;
 using Quartz;
 
@@ -30,62 +29,58 @@ namespace PlexNotifierr.Worker.Jobs
             var users = _dbContext.Users.Include(u => u.Medias).Include(us => us.Medias).ToList();
             var mediasRatingKey = _dbContext.Medias.Select(m => m.RatingKey).ToHashSet();
 
-            //Do it per user when option available in Plex.Api
-            var serverOwner = users.First(x => x.PlexId == 1);
-            var offset = serverOwner.HistoryPosition;
-            var limit = 100;
-            var isLastPage = false;
-            while (!isLastPage)
+            const int limit = 100;
+            foreach (var user in users)
             {
-                var history = await _serverClient.GetPlayHistory(_authToken, _url, offset, limit);
-                isLastPage = history.Size < limit;
-                offset += limit;
-                foreach (var historyMetadata in history.HistoryMetadata)
+                var offset = user.HistoryPosition;
+                var isLastPage = false;
+                while (!isLastPage)
                 {
-                    if (historyMetadata.Type != "episode") continue;
-                    //Get grandparentKey directly for HistoryMetada when field available in Plex.Api
-                    MediaContainer? episodeMediaContainer = await _serverClient.GetMediaMetadataAsync(_authToken, _url, historyMetadata.RatingKey);
-                    var episodeMetadata = episodeMediaContainer?.Media.FirstOrDefault();
-                    var grandparentRatingKey = episodeMetadata?.GrandparentRatingKey;
-                    var userConcerned = users.FirstOrDefault(x => x.PlexId == historyMetadata.AccountId);
-                    if (string.IsNullOrWhiteSpace(grandparentRatingKey) || episodeMetadata == null || userConcerned == null) continue;
-                    if (!mediasRatingKey.Contains(int.Parse(grandparentRatingKey)))
+                    var history = await _serverClient.GetPlayHistory(_authToken, _url, offset, limit, accountId: user.PlexId);
+                    isLastPage = history.Size < limit;
+                    offset += limit;
+                    if (history.HistoryMetadata is null) continue;
+                    foreach (var historyMetadata in history.HistoryMetadata.Where(historyMetadata => historyMetadata?.Type == "episode"))
                     {
-                        var grandParentMediaContainer = await _serverClient.GetMediaMetadataAsync(_authToken, _url, grandparentRatingKey);
-                        var grandParentMetadata = grandParentMediaContainer.Media.FirstOrDefault();
-                        var media = new Media()
+                        try
                         {
-                            RatingKey = int.Parse(grandparentRatingKey),
-                            Title = episodeMetadata.GrandparentTitle,
-                            Summary = grandParentMetadata?.Summary ?? "",
-                            ThumbUrl = grandParentMetadata?.Thumb ?? "",
-                        };
-                        _dbContext.Add(media);
-                        mediasRatingKey.Add(int.Parse(grandparentRatingKey));
-                        userConcerned.Medias.Add(new UserSubscription()
+                            var grandparentKey = historyMetadata.GrandParentKey;
+                            var grandparentRatingKey = grandparentKey?.Split('/').LastOrDefault() ?? string.Empty;
+                            if (!int.TryParse(grandparentRatingKey, out var grandParentRatingKey)) continue;
+                            if (!mediasRatingKey.Contains(grandParentRatingKey))
+                            {
+                                var grandParentMediaContainer = await _serverClient.GetMediaMetadataAsync(_authToken, _url, grandparentRatingKey);
+                                var grandParentMetadata = grandParentMediaContainer.Media.FirstOrDefault();
+                                if (grandParentMetadata is null) continue;
+                                var media = new Media()
+                                {
+                                    RatingKey = grandParentRatingKey,
+                                    Title = grandParentMetadata.Title ?? "",
+                                    ThumbUrl = grandParentMetadata.Thumb ?? "",
+                                    Summary = grandParentMetadata.Summary ?? "",
+                                };
+                                _dbContext.Add(media);
+                                mediasRatingKey.Add(grandParentRatingKey);
+                                _dbContext.UserSubscriptions.Add(new UserSubscription() { Media = media, User = user });
+                            }
+                            else
+                            {
+                                var userSubscription = user.Medias.FirstOrDefault(x => x.RatingKey == grandParentRatingKey);
+                                if (userSubscription is not null) continue;
+                                var media = _dbContext.Medias.Include(m => m.Users).FirstOrDefault(x => x.RatingKey == grandParentRatingKey);
+                                if (media is null) continue;
+                                _dbContext.UserSubscriptions.Add(new UserSubscription() { Media = media, User = user });
+                            }
+                        }
+                        catch (Exception e)
                         {
-                            Media = media,
-                            Active = true,
-                        });
+                            Console.WriteLine(e);
+                        }
                     }
-                    else
-                    {
-                        var userSubscription = userConcerned.Medias.FirstOrDefault(x => x.RatingKey == int.Parse(grandparentRatingKey));
-                        if (userSubscription == null) continue;
-                        var media = _dbContext.Medias.FirstOrDefault(x => x.RatingKey == int.Parse(grandparentRatingKey));
-                        if (media == null) continue;
-                        userConcerned.Medias.Add(new UserSubscription()
-                        {
-                            Media = media,
-                            Active = true,
-                        });
-                    }
-                    serverOwner.HistoryPosition = offset;
+                    user.HistoryPosition += history.Size;
+                    _ = await _dbContext.SaveChangesAsync();
                 }
-                _ = _dbContext.SaveChangesAsync();
             }
-
-
         }
     }
 }
